8d9e91c0f22bfaf3cc3007497e0ad6e285b59b8b
Demi Lee <demilee.to@gmail.com>
2017-10-04 14:06:57 -0400

    add the other pages

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+get '/' do
+  redirect to ('/home')
+end
+
+get '/gallery' do
+  redirect to ('/porfolio')
+end
+
 get '/home' do
   erb :index
 end
@@ -9,5 +17,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @links = { google: 'http://google.com', facebook: 'http://facebook.com', twitter: 'http://twitter/.com'}
+  erb :favourites
 end
